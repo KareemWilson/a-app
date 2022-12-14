c1a0887505d79c3248ac4fd1d164c61a79208630
KareemWilson <kareemmagdywilson@gmail.com>
2022-12-14 17:06:46 +0200

    Layout / Add text to hero section

--- src/components/section/Section.js
+++ src/components/section/Section.js
@@ -2,9 +2,7 @@ import * as React from "react"
 import * as styles from "./section.module.css"
 
 const Section = props => (
-  <>
     <div className={styles.section}>{props.children}</div>
-  </>
 )
 
 export default Section
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,12 @@ import Section from "../components/section/Section"
 const IndexPage = () => (
   <Layout>
     <Section name='Hero'>
-        <div>this is hero section</div>
-      </Section>
+        <p>Join in and connect</p>
+        <h1>To Faster your innovation potential</h1>
+        <p>AcceleratorApp Innovation Community — Tools and community to facilitate innovation between all members of our community</p>
+        <button>Schedule a Demo</button>
+        <img src="Video.svg" alt="template" style={ {position: 'absolute', top: '600px', left: '226px'} }/>
+    </Section>
   </Layout>
 )
 
