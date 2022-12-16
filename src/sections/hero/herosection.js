5fa25847d1f15e14c57ed07864d6f0767ddd7a36
KareemWilson <kareemmagdywilson@gmail.com>
2022-12-16 18:19:56 +0200

    Fix / Adjust layout in hero section

--- src/sections/hero/herosection.js
+++ src/sections/hero/herosection.js
@@ -5,34 +5,15 @@ import * as styles from "./heroSection.module.css"
 const HeroSection = props => (
   <Section additionalStyles={styles.hero}>
     <div className={styles.heroContent}>
-      <p style={{ fontSize: "26px", fontWeigth: "600", fontStyle: 'normal' }}>Join in and connect</p>
-      <h1
-        style={{
-          fontSize: "72px",
-          fontWeigth: "700",
-          lineHeight: "64px",
-          maxWidth: "50rem",
-        }}
-      >
+      <p>Join in and connect</p>
+      <h1>
         To faster your innovation potential
       </h1>
-      <p style={{ fontSize: "20px", fontWeigth: "100", maxWidth: "45rem", fontStyle: 'normal' }}>
+      <p>
         AcceleratorApp Innovation Community — Tools and community to facilitate
         innovation between all members of our community
       </p>
-      <button
-        style={{
-          width: "165px",
-          heigth: "48px",
-          backgroundColor: "white",
-          borderRadious: "4px",
-          padding: "12px 24px",
-          fontWeight: "600",
-          fontSize: "12px",
-          border: 'none',
-          borderRadius: '4px'
-        }}
-      >
+      <button className={styles.demoBtn}>
         Schedule a Demo
       </button>
     </div>
